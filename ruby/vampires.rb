f9a6bc6d79ab7dcb46a50df8c79058a805c10650
Brianna Forster <briannaforster@gmail.com>
2016-12-30 22:46:40 +0900

    Add interview questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name?"
+x = gets.chomp
+
+puts "How old are you?"
+x = gets.chomp
+
+puts "What year were you born?"
+x = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+x = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+x = gets.chomp
