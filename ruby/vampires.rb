1650acdeffb54956fe4594dfddb7036d70cd8a1d
Brianna Forster <briannaforster@gmail.com>
2016-12-30 23:39:48 +0900

    Add vampire detection logic

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,14 +1,58 @@
 puts "What is your name?"
-x = gets.chomp
+name = gets.chomp
 
 puts "How old are you?"
-x = gets.chomp
+age = gets.chomp.to_i
 
 puts "What year were you born?"
-x = gets.chomp
+born = gets.chomp.to_i
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-x = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+garlic = gets.chomp
 
-puts "Would you like to enroll in the company's health insurance?"
-x = gets.chomp
+puts "Would you like to enroll in the company's health insurance? (y/n)"
+insurance = gets.chomp
+
+if name = ("Drake Cula" || "Tu Fang")
+	name_ok = false
+	puts "Name check: failed!"
+else
+	name_ok == true
+	puts "Name check: cleared!"
+end
+
+if age == 2016-born || 2016-born-1
+	age_ok = true
+	puts "Age check: cleared!"
+else
+	age_ok = false
+	puts "Age check: failed!"
+end
+
+if garlic == "y"
+	garlic_ok = true
+	puts "Garlic check: cleared!"
+else
+	garlic_ok = false
+	puts "Garlic check: failed!"
+end
+
+if insurance == "y"
+	insurance_ok = true
+	puts "Mortality check: cleared!"
+else
+	insurance_ok = false
+	puts "Mortality check: failed!"
+end
+
+if age_ok == true && (garlic_ok == true || insurance_ok == true)
+	puts "Probably not a vampire."
+elsif age_ok == false && (garlic_ok == false || insurance_ok == false)
+	puts "Probably a vampire."
+elsif age_ok == false && garlic_ok == false && insurance_ok == false
+	puts "Almost certainly a vampire."
+elsif name_ok == false
+	puts "Definitely a vampire."
+else
+	puts "Results inconclusive."
+end
