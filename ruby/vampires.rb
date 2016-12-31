760f0f4c4de5b8d864fc7bb389022da6693799d7
Brianna Forster <briannaforster@gmail.com>
2016-12-31 14:33:51 +0900

    Add loop for allergy questions

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -18,49 +18,66 @@ until i == 0
 	puts "Would you like to enroll in the company's health insurance? (y/n)"
 	insurance = gets.chomp
 
-	if name != "Drake Cula" && name != "Tu Fang"
-		name_ok = true
-		puts "Name check: cleared!"
-	else
-		name_ok = false
-		puts "Name check: failed!"
+	n = 0
+	until n == "done"
+		puts "Name any allergies you may have, one by one. Enter 'done' when finished."
+		n = gets.chomp
+		if n == "sunshine"
+			break
+		end
 	end
 
-	if age == (2016 - born) || age == (2015 - born) 
-		# Accounting for people who have not yet had their birthday in 2016
-		age_ok = true
-		puts "Age check: cleared!"
+	if n == "sunshine"
+		puts "Sunshine check: failed. Probably a vampire."
 	else
-		age_ok = false
-		puts "Age check: failed!"
-	end
 
-	if garlic == "y"
-		garlic_ok = true
-		puts "Garlic check: cleared!"
-	else
-		garlic_ok = false
-		puts "Garlic check: failed!"
-	end
+		if name != "Drake Cula" && name != "Tu Fang"
+			name_ok = true
+			puts "Name check: cleared!"
+		else
+			name_ok = false
+			puts "Name check: failed!"
+		end
 
-	if insurance == "y"
-		insurance_ok = true
-		puts "Mortality check: cleared!"
-	else
-		insurance_ok = false
-		puts "Mortality check: failed!"
-	end
+		if age == (2016 - born) || age == (2015 - born) 
+			# Accounting for people who have not yet had their birthday in 2016
+			age_ok = true
+			puts "Age check: cleared!"
+		else
+			age_ok = false
+			puts "Age check: failed!"
+		end
 
-	if age_ok == true && (garlic_ok == true || insurance_ok == true)
-		puts "Probably not a vampire."
-	elsif age_ok == false && (garlic_ok == false || insurance_ok == false)
-		puts "Probably a vampire."
-	elsif age_ok == false && garlic_ok == false && insurance_ok == false
-		puts "Almost certainly a vampire."
-	elsif name_ok == false
-		puts "Definitely a vampire."
-	else
-		puts "Results inconclusive."
+		if garlic == "y"
+			garlic_ok = true
+			puts "Garlic check: cleared!"
+		else
+			garlic_ok = false
+			puts "Garlic check: failed!"
+		end
+
+		if insurance == "y"
+			insurance_ok = true
+			puts "Mortality check: cleared!"
+		else
+			insurance_ok = false
+			puts "Mortality check: failed!"
+		end
+
+		puts "Sunshine check: cleared!"
+
+
+		if age_ok == true && (garlic_ok == true || insurance_ok == true)
+			puts "Probably not a vampire."
+		elsif age_ok == false && (garlic_ok == false || insurance_ok == false)
+			puts "Probably a vampire."
+		elsif age_ok == false && garlic_ok == false && insurance_ok == false
+			puts "Almost certainly a vampire."
+		elsif name_ok == false
+			puts "Definitely a vampire."
+		else
+			puts "Results inconclusive."
+		end
 	end
 	i -= 1
 end
