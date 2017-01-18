0855920dd05b3c192f78723136021cb638af2f31
Brianna Forster <briannaforster@gmail.com>
2017-01-19 08:58:47 +0900

    Describe Puppy class traits

---
+++ ruby/puppy.rb
@@ -0,0 +1,13 @@
+# SPECIES:
+# Caninus Outerspaceus
+
+# CHARACTERISTICS:
+# Habitat: outer space
+# Number of heads: 2
+# Natural enemy: space cats, space mailmen
+# Will eat anything: yes
+
+# BEHAVIOR:
+# Attack hostile aliens on sight
+# Breathe in space
+# Move adeptly in zero gravity
