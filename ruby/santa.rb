5d880b1f464d101cd45c017ab8157d3b2fdd400c
Brianna Forster <briannaforster@gmail.com>
2017-01-21 22:58:39 +0900

    6.3 release 2

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -27,6 +27,33 @@ class Santa
 	def eat_milk_and_cookies(cookie_type)
 		puts "That was a good #{cookie_type}!"
 	end
+
+	def celebrate_birthday
+		@age += 1
+	end
+
+	def get_mad_at(reindeer_name)
+		#Change reindeer_name to last in @reindeer_ranking array
+		@reindeer_ranking.delete(reindeer_name)
+		@reindeer_ranking.insert(-1, reindeer_name)
+		#p @reindeer_ranking
+	end
+
+	def gender
+		@gender
+	end
+
+	def age=(new_age)
+		@age = new_age
+	end
+
+	def ethnicity=(new_ethnicity)
+		@ethnicity = new_ethnicity
+	end
+
+	def gender=(new_gender)
+		@gender = new_gender
+	end
 end
 
 # DRIVER CODE
@@ -43,8 +70,11 @@ puts "------"
 santa2 = Santa.new("female","humanoid")
 
 santa2.speak
+santa2.gender = "male"
 santa2.identity
+santa2.celebrate_birthday
 santa2.age
+santa2.get_mad_at("Dancer")
 santa2.least_fav_reindeer
 santa2.eat_milk_and_cookies("Oreo")
 
