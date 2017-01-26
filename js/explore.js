465e6b8b8eb76849fdaa65e0571d2daabd61d451
Brianna Forster <briannaforster@gmail.com>
2017-01-26 22:51:13 +0900

    Add pseudocode

---
+++ js/explore.js
@@ -0,0 +1,5 @@
+// Create a function that takes a string as its argument
+
+// Create a variable to store the reversed word
+
+// Loop backwards through each letter in the original word, adding each letter to the reversed word
