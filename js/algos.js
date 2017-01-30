7a8fdb23bb49b5d66801baa776574adadeaac280
Brianna Forster <briannaforster@gmail.com>
2017-01-31 00:58:16 +0900

    Release 0-1

--- js/algos.js
+++ js/algos.js
@@ -1,6 +1,7 @@
-//takes an array of words or phrases and returns the longest word or phrase in the array.
+// Release 0
+// takes an array of words or phrases and returns the longest word or phrase in the array.
 
-function longest_str(input_array){
+function longestStr(input_array){
 	// Push character count of each string to new array
 	var input_lengths = [];
 	for (i=0; i < input_array.length; i++){
@@ -16,9 +17,51 @@ function longest_str(input_array){
 }
 
 
+// Release 1
+// function that takes two objects and checks to see if the objects share at least one key-value pair
+
+
+function checkMatch(customer1, customer2){
+		if (customer1.name == customer2.name || customer1.age == customer2.age){
+	 		return true;
+	 	}
+	 	else{
+	 		return false;
+	 	}
+}
+
+// Release 2
+// takes an integer for length, and builds and returns an array of strings of the given length. 
+
+
 // DRIVER CODE
 
+// Release 0
 
-console.log(longest_str(["I", "am", "Brianna", "how", "about", "you?"]));
+console.log(longestStr(["I", "am", "Brianna", "how", "about", "you?"]));
+console.log(longestStr(["What", "a", "beautiful", "day"]));
 
-console.log(longest_str(["What", "a", "beautiful", "day"]));
+// Release 1
+
+// Names different, ages same - expect true
+var customer1 = {name: "Steven", age: 54};
+var customer2 = {name: "Tamir", age: 54};
+console.log(checkMatch(customer1, customer2));
+
+// Names same, ages different - expect true
+var customer1 = {name: "Steven", age: 54};
+var customer2 = {name: "Steven", age: 31};
+console.log(checkMatch(customer1, customer2));
+
+// Names and ages different - expect false
+var customer1 = {name: "Steven", age: 54};
+var customer2 = {name: "Tamir", age: 31};
+console.log(checkMatch(customer1, customer2));
+
+// Names and ages same - expect true
+var customer1 = {name: "Steven", age: 54};
+var customer2 = {name: "Steven", age: 54};
+console.log(checkMatch(customer1, customer2));
+
+// Release 2
+
