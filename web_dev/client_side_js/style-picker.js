fef1e5cf4305f892f2a67cacd4546242e224e7ce
Brianna Forster <briannaforster@gmail.com>
2017-02-12 15:24:05 -0800

    Add event listener

--- web_dev/client_side_js/style-picker.js
+++ web_dev/client_side_js/style-picker.js
@@ -11,9 +11,9 @@ function changeStyle() {
 		"lightcoral", "lightsteelblue", "maroon", "mediumpurple", "midnightblue", "navy", "palevioletred",
 		"plum", "peru", "sienna", "teal", "pink", "forestgreen", "deeppink", "brown", "khaki", 
 		"slategrey", "yellowgreen"];
-	var allFonts = ["Helvetica", "Arial Black", "Comic Sans MS", "Impact", "Lucida Sans Unicode", 
-		"Trebuchet MS", "Courier New", "Lucida Console", "Futura Medium", "Century Gothic Italic", 
-		"Rockwell Bold", "Baskerville Regular", "Didot Regular", "Garamond Regular"];
+	var allFonts = ["Helvetica", "Arial Black", "Impact", 
+		"Trebuchet MS", "Courier New", "Futura", "Gill Sans", "Copperplate",
+		"Baskerville", "Didot", "Garamond", "Palatino", "Tahoma"];
 	var allBorderStyles = ["none", "solid", "dotted", "dashed", "double", "groove", "ridge", "inset"];
 	var allBorderColors = ["aliceblue", "beige", "black", "DarkBlue", "DarkSeaGreen", "DarkSlateBlue", 
 		"DodgerBlue", "firebrick", "gainsboro", "indigo", "lightblue", "lavendar", "lightcyan", 
@@ -30,23 +30,8 @@ function changeStyle() {
 	var borderColor = allBorderColors[Math.floor(Math.random()*allBorderColors.length)];
 	var borderWidth = allBorderWidths[Math.floor(Math.random()*allBorderWidths.length)];
 
-	//Apply styles to each attribute
+	//Apply styles for each attribute
 
-/*
-
-	event.target.style.color = textColor;
-	event.target.style.backgroundColor = backgroundColor;
-	event.target.style.fontFamily = font;
-	event.target.style.borderStyle = borderStyle;
-	event.target.style.borderColor = borderColor;
-	event.target.style.borderWidth = borderWidth;
-
-	//console.log(textColor + ", " + backgroundColor + ", " + font + ", " + borderStyle + ", " + 
-	//	borderColor + ", " + borderWidth);
-
-*/
-	var pageText = document.getElementById("text-to-change");
-	console.log(pageText)
 
 	pageText.style.color = textColor;
 	pageText.style.backgroundColor = backgroundColor;
@@ -54,12 +39,12 @@ function changeStyle() {
 	pageText.style.borderStyle = borderStyle;
 	pageText.style.borderColor = borderColor;
 	pageText.style.borderWidth = borderWidth;
+	console.log(pageText);
 
 }
 
-//var pageText = document.getElementsbyTagName("em");
-//pageText.addEventListener("click", changeStyle);
+var pageText = document.getElementById("text-to-change");
+pageText.addEventListener("click", changeStyle);
 
 
-changeStyle();
 
