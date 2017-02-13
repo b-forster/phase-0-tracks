9d23ae7a2436935548e447b647ffbaff53807435
Brianna Forster <briannaforster@gmail.com>
2017-02-12 17:38:30 -0800

    Add new routes for 9.4 challenge

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,36 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+
+# RELEASE 0
+
+# #1
+get '/contact' do
+  "<p>Contact us at: <a href = 'mailto:brianna@jquery.com'>brianna@jquery.com</a></p>"
+end
+
+# #2
+
+get '/great_job' do
+  name = params[:name]
+  if name
+    "Good job, #{name}!"
+  else
+    "Good job!"
+  end
+end
+
+# #3
+
+get '/:num_1/:num_2' do
+ num_1 = params[:num_1].to_i
+ num_2 = params[:num_2].to_i
+ num_3 = num_1 + num_2
+ "<p>Your lucky number is #{num_3}!"
+end
+
+
+
+
+
